2854ec3b9734a4ed520ff9bfb14e1c22248b3bad
MatveyMelnikov <matveyy-melnikov@rambler.ru>
2022-09-13 23:22:19 +0300

    Adding test functionality and a hash algorithm

---
+++ src/MyEncryptionAlgorithm.cs
@@ -0,0 +1,27 @@
+﻿namespace EncryptionAlgorithms
+{
+    public class MyEncryptionAlgorithm : IEncryptionAlgorithm
+    {
+        public byte[] Decrypt(in byte[] data, in byte[] key)
+        {
+            byte[] result = (byte[])data.Clone();
+            byte[] currentKey = (byte[])key.Clone();
+
+            for (int i = 0; i < result.Length; i++)
+                result[i] = (byte)~result[i];
+
+            return result;
+        }
+
+        public byte[] Encrypt(in byte[] data, in byte[] key)
+        {
+            byte[] result = (byte[])data.Clone();
+            byte[] currentKey = (byte[])key.Clone();
+
+            for (int i = 0; i < result.Length; i++)
+                result[i] = (byte)~result[i];
+
+            return result;
+        }
+    }
+}
---
+++ src/MyHashAlgorithm.cs
@@ -0,0 +1,18 @@
+﻿using System.Security.Cryptography;
+
+namespace HashAlgorithms
+{
+    public class MyHashAlgorithm : IHashAlgorithm
+    {
+        public byte[] GetHash(in byte[] data, in byte[] salt)
+        {
+            byte[] currentData = (byte[])data.Clone();
+
+            for (int i = 0; i < currentData.Length; i++)
+                currentData[i] ^= salt[i];
+
+            var md5Hash = MD5.Create();
+            return md5Hash.ComputeHash(currentData);
+        }
+    }
+}
